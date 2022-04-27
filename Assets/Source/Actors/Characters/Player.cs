b630c1bd2be9eca105c85ac432426192713e92a1
Beelzseals <88729970+Beelzseals@users.noreply.github.com>
2022-04-27 13:54:46 +0200

    camera follows player

--- Assets/Source/Actors/Characters/Player.cs
+++ Assets/Source/Actors/Characters/Player.cs
@@ -6,44 +6,49 @@ namespace DungeonCrawl.Actors.Characters
 {
     public class Player : Character
     {
-        public (int x, int y) PlayerPosition { get => PlayerPosition; private set => PlayerPosition = value; }
+        //private (int x, int y) PlayerPosition;
+
         protected override void OnUpdate(float deltaTime)
         {
             if (Input.GetKeyDown(KeyCode.W))
             {
                 // Move up
                 TryMove(Direction.Up);
+                CameraController.Singleton.Position = this.Position;
             }
 
             if (Input.GetKeyDown(KeyCode.S))
             {
                 // Move down
                 TryMove(Direction.Down);
+                CameraController.Singleton.Position = this.Position;
             }
 
             if (Input.GetKeyDown(KeyCode.A))
             {
                 // Move left
                 TryMove(Direction.Left);
+                CameraController.Singleton.Position = this.Position;
             }
 
             if (Input.GetKeyDown(KeyCode.D))
             {
                 // Move right
                 TryMove(Direction.Right);
+                CameraController.Singleton.Position = this.Position;
             }
 
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                // Pick up or interact
-                Item item = ActorManager.Singleton.GetActorAt<Item>(Position);
-                PlayerPosition = item.Position;
-                if (item != null)
-                {
-                    Debug.Log(item.DefaultName);
-                    ActorManager.Singleton.DestroyActor(item);
-                }
-            }
+            //if (Input.GetKeyDown(KeyCode.E))
+            //{
+            //    // Pick up or interact
+            //    Item item = ActorManager.Singleton.GetActorAt<Item>(Position);
+            //    PlayerPosition = item.Position;
+            //    if (item != null)
+            //    {
+            //        Debug.Log(item.DefaultName);
+            //        ActorManager.Singleton.DestroyActor(item);
+            //    }
+            //}
         }
 
         public override bool OnCollision(Actor anotherActor)
@@ -55,6 +60,11 @@ namespace DungeonCrawl.Actors.Characters
         {
             Debug.Log("Oh no, I'm dead!");
         }
+
+        //public void SetPosition((int x, int y) position)
+        //{
+        //    PlayerPosition = position;
+        //}
 
         public override int DefaultSpriteId => 24;
         public override string DefaultName => "Player";
