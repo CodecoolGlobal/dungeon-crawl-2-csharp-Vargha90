42a01702b7526397eaaddd4f9c4b85448e520979
Beelzseals <88729970+Beelzseals@users.noreply.github.com>
2022-04-28 13:11:21 +0200

    add gateway classes (between maps)

---
+++ Assets/Source/Actors/Static/Gate.cs
@@ -0,0 +1,11 @@
+﻿using DungeonCrawl.Actors;
+
+namespace Assets.Source.Actors.Static
+{
+    internal class Gate : Actor
+    {
+        public override int DefaultSpriteId => 540;
+
+        public override string DefaultName => "Gate";
+    }
+}
---
+++ Assets/Source/Actors/Static/Stairs.cs
@@ -0,0 +1,11 @@
+﻿using DungeonCrawl.Actors;
+
+namespace Assets.Source.Actors.Static
+{
+    internal class Stairs : Actor
+    {
+        public override int DefaultSpriteId => 289;
+
+        public override string DefaultName => "Stairs";
+    }
+}
